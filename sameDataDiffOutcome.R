2a868226802ff4ac9436d281d7a48aa9fc949316
ramcharan <saitejapolisetti284@gmail.com>
2022-04-08 17:24:54 +0530

    visalize dataSaurus dataset

--- sameDataDiffOutcome.R
+++ sameDataDiffOutcome.R
@@ -17,3 +17,13 @@ ggplot(quartet, aes(x, y)) +
     geom_point() +
     geom_smooth(method = lm, se = FALSE) +
     facet_wrap(~set)
+
+
+install.packages('datasauRus')
+library('datasauRus')
+
+ggplot(datasaurus_dozen, aes(x = x, y = y, color = dataset)) +
+    geom_point() +
+    theme_void() +
+    theme(legend.position = "none") +
+    facet_wrap(~dataset)

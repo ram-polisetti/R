989f6f831da71c0fd26b7307e69a90f5c6e8c8e0
ramcharan <saitejapolisetti284@gmail.com>
2022-04-08 17:14:57 +0530

    create visualization for quartet

--- sameDataDiffOutcome.R
+++ sameDataDiffOutcome.R
@@ -3,6 +3,17 @@ library('Tmisc')
 data(quartet)
 View(quartet)
 
+install.packages('tidyverse')
+library('tidyverse')
+library('lubridate')
+
 quartet  %>% 
     group_by(set)  %>% 
     summarize(mean(x), sd(x), mean(y), sd(y), cor(x, y))
+
+library('ggplot2')
+
+ggplot(quartet, aes(x, y)) +
+    geom_point() +
+    geom_smooth(method = lm, se = FALSE) +
+    facet_wrap(~set)

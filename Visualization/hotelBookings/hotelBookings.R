72a77203b4afff688c72d0dc4b81c285882bf6df
ramcharan <saitejapolisetti284@gmail.com>
2022-04-10 20:56:48 +0530

    create new folder for hotel bookings

---
+++ Visualization/hotelBookings/hotelBookings.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("skimr")
+install.packages("janitor")
+
+library(tidyverse)
+library(skimr)
+library(janitor)
+
+hotel_bookings <- read.csv("hotel_bookings.csv")

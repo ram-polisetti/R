b88bd4c8a59b052926826e442bff44fe34087770
ramcharan <saitejapolisetti284@gmail.com>
2022-04-12 20:34:05 +0530

    create new visualizations using hotelbookings data

--- Visualization/hotelBookings/hotelBookings.R
+++ Visualization/hotelBookings/hotelBookings.R
@@ -6,4 +6,20 @@ library(tidyverse)
 library(skimr)
 library(janitor)
 
+
 hotel_bookings <- read.csv("hotel_bookings.csv")
+head(hotel_bookings)
+
+head(hotel_bookings)
+
+colnames(hotel_bookings)
+
+install.packages('ggplot2')
+library('ggplot2')
+
+# Check whether people with children can book hotel rooms in advance or not
+ggplot(data = hotel_bookings) +
+    geom_point(mapping = aes(x = lead_time, y = children))
+
+ggplot(data = hotel_bookings) +
+    geom_point(mapping = aes(x = stays_in_weekend_nights, y = children))
